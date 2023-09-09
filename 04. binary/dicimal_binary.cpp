f4e395ccf67cbee07ce409e844ae716210cac0bd
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-09 14:24:37 +0530

    passByvalue

--- 04. binary/dicimal_binary.cpp
+++ 04. binary/dicimal_binary.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+int update(int a)
+{
+    a -= 5;
+    cout << "dummy" << a << endl;
+    return a;
+}
 int main()
 {
-    int i = 0;
-    int ans = 0; 
-    int n = 4;
-    while (n != 0)
-    {
-        int bit = n & 1;
-        ans = (bit * pow(10, i)) + ans;
-        n = n>>1;
-        i++;
-    }
-    cout<<"Binary value is :"<<ans<<endl;
+    // int i = 0;
+    // int ans = 0;
+    // int n = 4;
+    // while (n != 0)
+    // {
+    //     int bit = n & 1;
+    //     ans = (bit * pow(10, i)) + ans;
+    //     n = n>>1;
+    //     i++;
+    // }
+    // cout<<"Binary value is :"<<ans<<endl;
+    int a = 15;
+    // update(a);
+    cout << "main" << update(a) << endl;
     return 0;
 }
