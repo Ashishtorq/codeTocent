eceee56951a71feb5998bbd757c96874c4384c54
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-14 16:51:44 +0530

    swap

---
+++ 6. Array/01_swapalter.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    int i = 0;
+    int j = i + 1;
+    while (j > 5)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i += 2;
+        j += 2;
+    }
+    for(int i = 0; i<6; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
