3e351c8f70429a1a7efd6240e7f6b9d03c1b47e3
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-15 23:26:53 +0530

    comment

--- 6. Array/05. sort01.cpp
+++ 6. Array/05. sort01.cpp
@@ -1,20 +1,20 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main()
-{
-    int arr[7] = {0,1,0,1,1,0,1};
-    int i = 0, j = 6;
-    while(i<j){
-        if(arr[i] == 0) i++;
-        if(arr[j] == 1) j--;
-        if(arr[i]==1 && arr[j] == 0){
-            swap(arr[i],arr[j]);
-            i++,j--;
-        }
-    }
-    for(int i = 0; i<7; i++){
-        cout<<arr[i]<<" ";
-    }
-    return 0;
-}
+// int main()
+// {
+//     int arr[7] = {0,1,0,1,1,0,1};
+//     int i = 0, j = 6;
+//     while(i<j){
+//         if(arr[i] == 0) i++;
+//         if(arr[j] == 1) j--;
+//         if(arr[i]==1 && arr[j] == 0){
+//             swap(arr[i],arr[j]);
+//             i++,j--;
+//         }
+//     }
+//     for(int i = 0; i<7; i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     return 0;
+// }
