395bb7b876014b59e2bf15f342c4e3e13e2f8f6e
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-17 01:15:39 +0530

    bs

--- 07. Binary Search/02.firstOccurance.cpp
+++ 07. Binary Search/02.firstOccurance.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
-int firstOccurace(int arr[], int n, int k)P
+int firstOccurace(int arr[], int n, int k){
+    int s = 0;
+    int e = n-1;
+    int mid = (s+e)/2;
+    int ans = -1;
+    while(s<=e){
+        if(arr[mid] == k){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (k>arr[mid]){
+            s = mid + 1;
+        }
+        else e = mid -1;
+        mid = (s+e)/2;
+    }
+    return ans;
+}
+
 int main()
 {
     int arr[6] = {5,7,7,8,8,10};
-    firstOccurace
+    cout<<firstOccurace(arr,6,8);
     return 0;
 }
