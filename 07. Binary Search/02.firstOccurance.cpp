a5e60a370b958b82984abc64e4979d497b7f58f9
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-17 01:38:47 +0530

    last occurance

--- 07. Binary Search/02.firstOccurance.cpp
+++ 07. Binary Search/02.firstOccurance.cpp
@@ -18,10 +18,28 @@ int firstOccurace(int arr[], int n, int k){
     }
     return ans;
 }
-
+int lastOccurace(int arr[], int n, int k){
+    int s = 0;
+    int e = n-1;
+    // int mid = (s+e)/2;
+    int ans = -1;
+    while(s<=e){
+        int mid = (s+e)/2;
+        if(arr[mid] == k){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (k>arr[mid]){
+            s = mid + 1;
+        }
+        else e = mid -1;
+    }
+    return ans;
+}
 int main()
 {
     int arr[6] = {5,7,7,8,8,10};
-    cout<<firstOccurace(arr,6,8);
+    cout<<"The first occurance is at index "<<firstOccurace(arr,6,8)<<endl;
+    cout<<"The last occurance is at index "<<lastOccurace(arr,6,8)<<endl;
     return 0;
 }
