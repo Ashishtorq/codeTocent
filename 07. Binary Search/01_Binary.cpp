c9200dfa7adcfe81f015026683e78567ad8fb93e
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-16 22:34:08 +0530

    binary

--- 07. Binary Search/01_Binary.cpp
+++ 07. Binary Search/01_Binary.cpp
@@ -1,8 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    
-    return 0;
+    int arr[6] = {2, 6, 8, 10, 12, 20};
+    int s = 0;
+    int e = 5;
+    int mid = (s + e) / 2;
+    int x = 10;
+    while (s <= e)
+    {
+
+        if (arr[mid] == x)
+            {cout << "Element is found at : " << mid;}
+        else if (mid > x)
+           { e = mid - 1;}
+        else
+            {s = mid + 1;}
+        mid = (s + e) / 2;
+    }
+    cout << "Element not found" << endl;
 }
