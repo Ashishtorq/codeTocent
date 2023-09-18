701e9f17fc0d3bea4cf2f2db0624f791f72e7b85
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-19 02:02:10 +0530

    min ele

--- 07. Binary Search/04. min_eleme.cpp
+++ 07. Binary Search/04. min_eleme.cpp
@@ -1,23 +1,31 @@
-
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    vector<int>nums{1,2,3,4,5};
-    vector<int>pre(nums.size(),0);
-    pre[0] = nums[0];
-    for(int i = 1; i<nums.size(); i++) {
-        pre[i] = nums[i] + pre[i-1];
+    int n;
+    cout << "Enter size of array" << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the element in array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    for(auto val :pre)cout<<val<<" " ;
-    cout<<endl ;
-    
-    int s,e;
-    cin>>s>>e;
-    int ans  = pre[e] - pre[s-1];
-    cout<<ans;
+    int s = 0, e = n - 1;
+    int mid = (s + e) / 2;
+    int ans;
+    while (s < e)
+    {
+        if (arr[mid] > arr[0])
+        {
+            s = mid + 1;
+            ans = mid;
+        }
+        else if (arr[mid]<arr[0]){
+            e = mid;
+        }
+    }
 
-    return 0;
+    
 }
