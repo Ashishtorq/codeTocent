057efb09d1fddbf63097cbb8cccbbf7c42296df5
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-25 03:08:39 +0530

    string

---
+++ 09. String/01.String.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char c[10];
+    cin>>c;
+    return 0;
+}
---
+++ 09. String/02.Length.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch[20];
+   cout<<"Enter the character"<<endl;
+   cin>>ch;
+   int count = 0;
+   for(int i=0; ch[i]!='\0'; i++){
+       count++;
+   }
+   cout<<"Length of char is : "<<count<<endl;
+   
+    return 0;
+}
