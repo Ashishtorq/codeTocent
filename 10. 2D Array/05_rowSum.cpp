f985a23f20ddf08cd931a6982e0ff6a83fb8ac73
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-27 15:11:28 +0530

    sum of row

---
+++ 10. 2D Array/05_rowSum.cpp
@@ -0,0 +1,22 @@
+ #include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3][4];
+    for(int i=0 ;i<3; i++){
+        for(int j=0; j<4; j++){
+            cin>>arr[i][j];
+        }
+    }
+    int rowSum = 0;
+    for(int i=0 ;i<3; i++){
+        for(int j=0; j<4; j++){
+            rowSum+=arr[i][j];
+        }
+        cout<<rowSum<<endl;
+        rowSum = 0;
+        cout<<endl;
+    }
+    return 0;
+}
