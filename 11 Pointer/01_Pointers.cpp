c89daa646e0e50e70de6a8c2bc2e57370f261b05
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-01 02:02:20 +0530

    copy pointer

--- 11 Pointer/01_Pointers.cpp
+++ 11 Pointer/01_Pointers.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main()
 {
-    int num = 5;
-    int *ptr = &num;
-    cout << &num << endl;
-    cout << *ptr << endl;
-    (*ptr)++;
-    cout << *ptr << endl;
+    // int num = 5;
+    // int *ptr = &num;
+    // cout << &num << endl;
+    // cout << *ptr << endl;
+    // (*ptr)++;
+    // cout << *ptr << endl;
+    int n = 5;
+    int *p = &n;
+    int *q = p;
+    cout<<p<<" - "<<*p<<" - "<<q<<" - "<<*q<<endl;
 
     return 0;
 }
