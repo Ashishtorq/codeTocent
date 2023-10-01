4656587cb9c40c1685a464190fe4e447762b2394
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-02 00:22:38 +0530

    pass by value

--- 11 Pointer/03_passbyValue.cpp
+++ 11 Pointer/03_passbyValue.cpp
@@ -1,8 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+void upDate(int i)
+{
 
+    i++;
+}
+void update(int& i)
+{
+
+    i++;
+}
 int main()
 {
-    
+    int i = 5;
+    cout << "Before" << i << endl;
+    upDate(i);
+    cout << "Before" << i << endl;
+
+    int n = 6;
+    cout << "Before" << i << endl;
+    update(n);
+    cout << "Before" << i << endl;
     return 0;
 }
