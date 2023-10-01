6e42a7e1011286dc046ded73cf2d8254720fd48b
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-01 21:43:54 +0530

    pointers

--- 11 Pointer/02_pointer.cpp
+++ 11 Pointer/02_pointer.cpp
@@ -1,14 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int i=5;
+    int i = 5;
     int *p = &i;
     int **p2 = &p;
 
-    cout<< i <<endl;
-    cout<< *p <<endl;
-    cout<< **p <<endl;
+    cout << i << endl;
+    cout << *p << endl;
+    cout << **p2 << endl;
+    cout << p2 << endl;
+    cout << p << endl;
     return 0;
 }
