afd55d841b5faac8b8c571541e8946e7723a4792
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-02 21:36:22 +0530

    recursion

---
+++ 12 Recursion/01_Factorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+    int f = 1;
+    for (int i = n; i >= 1; i--)
+    {
+        f = f * i;
+    }
+    return f;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter n" << endl;
+    cin >> n;
+    cout << "Factorial : " << fact(n);
+    return 0;
+}
