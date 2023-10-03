6b9b42816cf27b81ead3f83cf34a7f94ce345aef
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-04 00:28:05 +0530

    counting in recursion

---
+++ 12 Recursion/03_Count.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int count(int n)
+{
+
+    if (n == -1)
+        return true;
+    cout << n << ", ";
+    return count(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter value of n" << endl;
+    cin >> n;
+    int ans = count(n);
+    // cout << "Ans : " << ans;
+    return 0;
+}
