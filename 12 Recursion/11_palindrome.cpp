9d61212ef421d9618071c29827bce0ff0bdbc17c
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-04 23:52:38 +0530

    palidrome in recursion

---
+++ 12 Recursion/11_palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+bool palindrome(string &s, int i, int j)
+{
+
+    cout << "Call received for : " << s << endl;
+
+    if (i > j)
+        return 1;
+    if(s[i]!=s[j]) return false;
+    else;
+    i++, j--;
+    return palindrome(s, i, j);
+    // return ans;
+}
+
+int main()
+{
+    string s = "abca";
+    int i = 0, j = s.length() - 1;
+    cout<<"ans : "<<palindrome(s, i, j);
+
+    return 0;
+}
