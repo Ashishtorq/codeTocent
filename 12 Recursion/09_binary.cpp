5cdf116b3ae0dea9d925716b845a1ad01566cb70
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-04 20:35:49 +0530

    recursion in binary search

--- 12 Recursion/09_binary.cpp
+++ 12 Recursion/09_binary.cpp
@@ -1,26 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool binary(int arr[],int s, int e,int x){
-    int mid = (s+e)/2;
-    if(s>e) return false;
-    if(arr[mid]==x) return true;
-    else if(arr[mid]>x){
-        binary(arr,s,mid-1,x);
+bool binary(int *arr, int s, int e, int x)
+{
+    if (s > e)
+        return false;
+    int mid = (s + e) / 2;
+    if (arr[mid] == x)
+        return true;
+    else if (arr[mid] > x)
+    {
+        binary(arr, s, mid - 1, x);
     }
-    else if (arr[mid]<x){
-        binary(arr, mid+1, e, x);
+    else
+    {
+        binary(arr, mid + 1, e, x);
     }
 }
 
 int main()
 {
-    int arr[6] = {2,3,6,8,10,12};
+    int arr[6] = {2, 3, 6, 8, 10, 12};
     int s = arr[0];
     int e = arr[5];
     int x = 11;
-    int ans = binary(arr,s,e,x);
-    if(ans) cout<<"Element is present"<<endl;
-    else cout<<"not found"<<endl;
+    // cout<<"Ans : "<<binary(arr,s,e,x);
+    if (binary(arr, s, e, x))
+        cout << "Element is present" << endl;
+    else
+        cout << "not found" << endl;
     return 0;
 }
