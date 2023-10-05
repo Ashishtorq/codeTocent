c58f8d9d1ade1daf75cad5039bb9577f8d1fc3a6
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-06 01:52:44 +0530

    power using recursion

---
+++ 12 Recursion/13_power.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int power(int a, int b){
+    if(b==0) return 1;
+    if(b==1) return a;
+
+    int ans =  power(a,b/2);
+    if(b%2==0){
+        return ans *ans;
+    }
+    else return a * ans * ans;
+}
+int main()
+{
+    int a;
+    cout<<"Enter a"<<endl;
+    cin>>a;
+    int b;
+    cout<<"Enter b"<<endl;
+    cin>>b;
+
+    int ans = power(a,b);
+    cout<<"Ans - "<<ans;
+    return 0;
+}
