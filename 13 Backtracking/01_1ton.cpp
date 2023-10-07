e6bccee00c6dcfdaadb80530ea8ae945fdf7d69e
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-08 01:32:50 +0530

    backtracking problems

---
+++ 13 Backtracking/01_1ton.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void f(int i, int n){
+    if(i<1)return;
+    f(i-1,n);
+    cout<<i<<" ";
+}
+int main()
+{
+    int n;
+    cout<<"Enter the value of n"<<end;
+    f(n,n);
+    return 0;
+}
