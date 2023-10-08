470f99d5ed80c52e62f64499a9ebfd3f9eb90ba4
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-09 04:21:40 +0530

    recursion sum

--- 12 Recursion/16_subset_sum.cpp
+++ 12 Recursion/16_subset_sum.cpp
@@ -1,32 +1,42 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-//bool flag = true;
-void helper(int arr[],int n, vector<int>temp, int index, int s, int sum){
-    if(index==n){
-        if(s==sum){
-           // flag = false;
-            for(auto x:temp) cout<<x;
-            cout<<endl;
+
+bool helper(int arr[], int n, vector<int> temp, int index, int s, int sum)
+{
+    if (index == n)
+    {
+        if (s == sum)
+        {
+            for (auto x : temp)
+                cout << x;
+            cout << endl;
+            return true;
         }
-        return;
+        else
+            return false;
     }
-    // pick 
-    temp.push_back(arr[index]); 
+    // pick
+    temp.push_back(arr[index]);
     s = s + arr[index];
-    helper(arr, n, temp, index+1, s, sum);
-    // if(helper == true) return;
+    helper(arr, n, temp, index + 1, s, sum);
+    if (helper(arr, n, temp, index + 1, s, sum) == true)
+        return true;
     // not pick
     temp.pop_back();
-    s  = s - arr[index];
-    helper(arr, n, temp, index+1, s, sum);
+    s = s - arr[index];
+    helper(arr, n, temp, index + 1, s, sum);
+    helper(arr, n, temp, index + 1, s, sum);
+    if (helper(arr, n, temp, index + 1, s, sum) == true)
+        return true;
+    return false;
 }
 int main()
 {
-    int arr[] = {1,2};
-    int sum = 3 ;
+    int arr[] = {1, 2};
+    int sum = 3;
     int n = 3;
-    vector<int>temp;
-    helper(arr,n,temp,0,0,sum);
+    vector<int> temp;
+    helper(arr, n, temp, 0, 0, sum);
     return 0;
 }
