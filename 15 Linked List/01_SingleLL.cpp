7e535772a32bd849905d4e86ead104a438794d9c
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-13 00:22:23 +0530

    linkedlist

--- 15 Linked List/01_SingleLL.cpp
+++ 15 Linked List/01_SingleLL.cpp
@@ -1,17 +1,28 @@
 #include<iostream>
 using namespace std;
-struct node{
+// struct node{
+//     int data;
+//     node *next;
+//     node(int d){
+//         data = d ;
+//         next = nullptr;
+//     }
+// };
+class node{
+    public:
     int data;
-    node *next;
+    node * next;
     node(int d){
-        data = d ;
-        next = nullptr;
+        this -> data = d;
+        this -> next = nullptr;
     }
 };
 int main()
 {
     node *head = new node(2);
-    head->next = new node(3);
-
+     head->next = new node(3);
+     head->next->next = new node(5);
+    cout<<head->next->next->data;
+    cout<<head->next->data;
     return 0;
 }
