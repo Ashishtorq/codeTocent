2f4280976035084adb9eef6c0942d3d9fd89e811
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-12 19:13:54 +0530

    oops

--- 14 OOPS/01_cls_obj.cpp
+++ 14 OOPS/01_cls_obj.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
 class Hero{
-    // int health;
-    // int h;
+     public:
+    int health;
+    int level;
 };
 int main()
 {
     Hero h;
-    cout<<sizeof(h);
+    cout<<sizeof(h)<<endl;
+    cout<<"helth"<<h.health<<endl;
+    cout<<"level"<<h.level<<endl;
     return 0;
 }
