8a46f0e3ad75b249c9c7d9cf1c961ce40094a0d4
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-13 17:33:27 +0530

    insert at head

--- 15 Linked List/02_insert@head.cpp
+++ 15 Linked List/02_insert@head.cpp
@@ -1,23 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class node{
-    public:
+class node
+{
+public:
     int data;
-    node*next;
-    node(int data){
-        this -> data = data;
-        this -> next = nullptr;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
     }
 };
-    void insertAtHead(node* &head,int data){
-        node* temp = new node(data);
-        temp -> next = head;
-        temp = head;
+void insertAtHead(node *&head, int data)
+{
+    node *temp = new node(data);
+    temp->next = head;
+    head = temp;
+}
+void print(node *&head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
     }
+    cout << endl;
+}
 int main()
 {
-    node *head = new node(2);
+    node *head = new node(10);
     // head->next = new node(6);
-
+    print(head);
+    insertAtHead(head, 12);
+    print(head);
+    insertAtHead(head,13);
+    print(head);
     return 0;
 }
