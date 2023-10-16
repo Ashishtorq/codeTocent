deb6e8bc970fa4744e20fae8acf7c9dc26b8c249
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-17 00:40:03 +0530

    array

--- 17 Circular LL/01_CLL.cpp
+++ 17 Circular LL/01_CLL.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 using namespace std;
-
+class node{
+    public:
+    int data;
+    node*next;
+    node(int data){
+        this -> data = data;
+        this -> next = nullptr;
+    }
+};
 int main()
 {
-    
+    node* head = new node(1);
+    head->next = new node(2);
     return 0;
 }
