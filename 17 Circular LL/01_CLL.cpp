8a338bb48dcf175a29ffbe53501d9e2ed1fc45ea
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-17 01:37:59 +0530

    circular likedlist

--- 17 Circular LL/01_CLL.cpp
+++ 17 Circular LL/01_CLL.cpp
@@ -9,9 +9,38 @@ class node{
         this -> next = nullptr;
     }
 };
+void print(node* &tail){
+    node *temp = tail;
+    while(temp!=nullptr){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL";
+    cout<<endl;
+}
+void insert(node* &tail, int element, int data){
+    // empty list
+    if(tail == nullptr){
+        node* newnode = new node(data);
+        tail = newnode;
+        newnode ->next = newnode;
+    }
+    // non-empty list.(assuming element is present)
+    else{
+        node* currEle = tail;
+        while(currEle->data != element){
+            currEle = currEle -> next;
+        }
+    // we got the element and currEle is reprenting it
+    node*temp = new node(data);
+    temp -> next = currEle->next;
+    currEle->next = temp;
+    }
+}
 int main()
 {
-    node* head = new node(1);
-    head->next = new node(2);
+    node*tail = nullptr;
+    insert(tail,5,3);
+    print(tail);
     return 0;
 }
