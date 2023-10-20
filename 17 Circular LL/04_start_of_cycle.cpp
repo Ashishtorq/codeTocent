c03e25654b05a6ef33682d0b10d88cb2f97cc7fa
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-20 21:05:39 +0530

    input in ll

---
+++ 17 Circular LL/04_start_of_cycle.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+bool floydDetect(node *head)
+{
+    if (head == nullptr)
+    {
+        return false;
+    }
+    node *slow = head;
+    node *fast = head;
+    while (fast != nullptr)
+    {
+        fast = fast->next;
+        if (fast != nullptr)
+        {
+            fast = fast->next;
+        }
+        slow = slow->next;
+        if (slow == fast)
+        {
+            cout << "Cycle present at -> " << slow->data << endl;
+            return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    node *head = new node(2);
+    head->next = new node(10);
+    head->next->next = new node(5);
+    head->next->next->next = new node(10);
+    head->next->next->next->next = new node(5);
+
+    if (floydDetect(head))
+        cout << "loop" << endl;
+    else
+        cout << "no loop";
+    return 0;
+}
---
+++ 17 Circular LL/05_Taking_input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node*next;
+    node(int data){
+        this ->data = data;
+        this -> next = nullptr;
+    }
+};
+int main()
+{
+    
+    return 0;
+}
