1a30e6f0dcbda1db1c92cac829c6ece0938aea4c
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-27 22:43:27 +0530

    stack implement

--- 18 Stack/02_stackImplement.cpp
+++ 18 Stack/02_stackImplement.cpp
@@ -1,43 +1,67 @@
-#include<iostream>
+#include <iostream>
+// #include<stack>
 using namespace std;
-class stack{
+class stack
+{
+public:
     int top;
     int *arr;
     int size;
 
-    stack(int size){
-        this -> size = size;
+    stack(int size)
+    {
+        this->size = size;
         arr = new int[size];
         top = -1;
     }
-    void push(int element){
-        if(size - top > 1){
+    void push(int element)
+    {
+        if (size - top > 1)
+        {
             top++;
             arr[top] = element;
         }
-        else{
-            cout<<"Stack Overflow"<<endl;
+        else
+        {
+            cout << "Stack Overflow" << endl;
         }
     }
-    void pop(){
-        if(top >= 0){
+    void pop()
+    {
+        if (top >= 0)
+        {
             top--;
         }
-        else {
-            cout<<"stack underflow"<<endl;
+        else
+        {
+            cout << "stack underflow" << endl;
         }
     }
-    int peek(){
-        if(top>=0 and top<size){
+    int peek()
+    {
+        if (top >= 0 and top < size)
+        {
             return top;
         }
-        else {
-            cout<<"stack is empty"<<endl;
+        else
+        {
+            cout << "stack is empty" << endl;
         }
     }
-    bool isEmpty(int top){
-        if(top == -1) return 0;
-        else return 0;
+    void print()
+    {
+        if (top == -1)
+            return;
+        for (int i = 0; i <= top; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+    bool isEmpty(int top)
+    {
+        if (top == -1)
+            return 0;
+        else
+            return 0;
     }
 };
 int main()
@@ -46,5 +70,8 @@ int main()
     st.push(1);
     st.push(1);
     st.push(1);
+
+    st.print();
+
     return 0;
 }
