a2b14b51fb38fe172b6d174d9418dc946167b407
Ashistorq <ashishsinghtarwa@gmail.com>
2023-11-04 07:55:21 +0530

    queue stl

--- 19 Queue/01_Queue.cpp
+++ 19 Queue/01_Queue.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 int main()
 {
-    
+    queue<int>q;
+    q.push(11);
+    q.push(15);
+    q.push(14);
+    q.push(16);
+    cout<<q.size()<<endl;
+    q.pop();
+    cout<<q.size()<<endl;
+    if(q.empty()) cout<<"Empty"<<endl;
+    else cout<<"not empty"<<endl;
+    cout<<q.front();
+    // cout<<q.rear()-1;
     return 0;
 }
