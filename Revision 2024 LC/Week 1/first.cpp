0d13c7fcbfdddaab5fe1469a5983c4c26a214082
Ashistorq <ashishsinghtarwa@gmail.com>
2024-05-03 10:41:49 +0530

    Add Sum function to calculate the sum of two numbers in first.cpp

---
+++ Revision 2024 LC/Week 1/first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+    int Sum(int a, int b){
+        int ans = a + b;
+        return ans;
+    }
+int main()
+{
+
+    cout<<Sum(10,15);
+    return 0;
+}
